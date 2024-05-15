3d0b8180382bcdb2954e25d03ee0b10cf9fc8252
dollyaggarwal123 <dolly.aggarwal@oodles.io>
2024-05-15 13:45:11 +0530

    Integrated Dashboard App

--- MFE_Base/MFE_Base/src/App.jsx
+++ MFE_Base/MFE_Base/src/App.jsx
@@ -4,7 +4,8 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./index.scss";
 import Register from "../../../register/register/src/Register";
 import Login from "../../../login/login/src/Login";
-import Container from "../../../dashboard/src/Components/Container";
+// import Container from "../../../dashboard/src/Components/Container";
+import {App as Dashboard} from "../../../dashboard/src/App";
 
 const root = ReactDOM.createRoot(document.getElementById("app"));
 
@@ -15,7 +16,7 @@ const App = () => (
     
     <Route path="/register" element={<Register/>}/>
     <Route path="/" element={<Login/>}/>
-    <Route path="/dashboard" element={<Container/>}/>
+    <Route path="/dashboard" element={<Dashboard/>}/>
     </Routes>
     </BrowserRouter>
     
